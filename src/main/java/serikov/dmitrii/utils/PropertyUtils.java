9a892a1598a35e39cadb8ced1c87af08117df26a
DmitriiSer <dmitrii.serikov@gmail.com>
2017-09-27 22:34:00 -0400

    Removed guava dependency. Using forEach() instead.

--- src/main/java/serikov/dmitrii/utils/PropertyUtils.java
+++ src/main/java/serikov/dmitrii/utils/PropertyUtils.java
@@ -2,31 +2,32 @@ package serikov.dmitrii.utils;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
-import com.google.appengine.repackaged.com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Maps;
-
 public class PropertyUtils {
 
-	private static final String APP_PROPERTIES_RESOURCE = "/resources/app.properties";
+	private static final Logger logger = LoggerFactory.getLogger(PropertyUtils.class);
 
-	private static Map<String, String> propertiesStore;
+	private static final String APP_PROPERTIES_RESOURCE = "/WEB-INF/classes/app.properties";
+
+	private static Map<String, String> propertiesStore = new HashMap<>();
 
 	public static Map<String, String> readAppProperties() {
-		if (propertiesStore != null) {
+		if (!propertiesStore.isEmpty()) {
 			return propertiesStore;
 		}
 		try (InputStream inputStream = PropertyUtils.class.getClassLoader()
 				.getResourceAsStream(APP_PROPERTIES_RESOURCE)) {
-			Properties prop = new Properties();
-			prop.load(inputStream);
-
-			propertiesStore = Maps.fromProperties(prop);
-			
-		} catch(IOException e){
-			propertiesStore = ImmutableMap.of();
+			if (inputStream != null) {
+				Properties prop = new Properties();
+				prop.load(inputStream);
+				prop.forEach((key, value) -> propertiesStore.put(key.toString(), value.toString()));
+			} else {
+				logger.error(String.format("Cannot find \"%s\"", APP_PROPERTIES_RESOURCE));
+			}
+		} catch (IOException e) {
 		}
 		return propertiesStore;
 	}
