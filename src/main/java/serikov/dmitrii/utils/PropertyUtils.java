bad77213b1986ce9118a299aaa83e9732b85dcf7
DmitriiSer <dmitrii.serikov@gmail.com>
2017-09-27 22:53:58 -0400

    Changed 'readAppProperties()' to 'loadAppProperties()' method. Added 'getAppProperties()' method

--- src/main/java/serikov/dmitrii/utils/PropertyUtils.java
+++ src/main/java/serikov/dmitrii/utils/PropertyUtils.java
@@ -6,20 +6,18 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
+import javax.servlet.ServletContext;
+
 public class PropertyUtils {
+
+	public static final String APP_PROPERTIES_RESOURCE = "/WEB-INF/classes/app.properties";
 
 	private static final Logger logger = LoggerFactory.getLogger(PropertyUtils.class);
 
-	private static final String APP_PROPERTIES_RESOURCE = "/WEB-INF/classes/app.properties";
-
 	private static Map<String, String> propertiesStore = new HashMap<>();
 
-	public static Map<String, String> readAppProperties() {
-		if (!propertiesStore.isEmpty()) {
-			return propertiesStore;
-		}
-		try (InputStream inputStream = PropertyUtils.class.getClassLoader()
-				.getResourceAsStream(APP_PROPERTIES_RESOURCE)) {
+	public static void loadAppProperties(ServletContext context) {
+		try (InputStream inputStream = context.getResourceAsStream(PropertyUtils.APP_PROPERTIES_RESOURCE)) {
 			if (inputStream != null) {
 				Properties prop = new Properties();
 				prop.load(inputStream);
@@ -29,6 +27,9 @@ public class PropertyUtils {
 			}
 		} catch (IOException e) {
 		}
+	}
+	
+	public static Map<String, String> getAppProperties(){
 		return propertiesStore;
 	}
 
